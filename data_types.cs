24eee99b0abd532d1bef5a46dfd34f7d57d48a59
ashwinighodake <ashwini752000@gmail.com>
2022-02-28 18:01:34 +0530

    First application

---
+++ data_types.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Datatypes
+{
+    class Data_types
+    {
+        static void Main(String []args)
+        {
+            int no;
+            double no2;
+            char var;
+            bool flag;
+            string name;
+
+            no=10;
+            no2=90.00D;
+            var='a';
+            flag=true;
+            name="Its me";
+
+            Console.WriteLine("int value:"+no+"\nDouble value:"+no2+"\nchar value:"+var+"\nboolean value:"+flag+"\nString Value:"+name);
+        }
+    }
+}
